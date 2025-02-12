759d0bb275ac714d0295a37d6af3c4dba798e84c
unallami <unallami@asu.edu>
2025-02-11 18:14:19 -0700

    Task #26 : Created Basic Webpage Home Screen with Title

---
+++ Front End/src/components/HomeScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Metric Calculator</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center; /* Centers the text horizontally */
+  padding-top: 20px; /* Adds space from the top */
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: bold;
+  margin: 0; /* Removes default margin */
+}
+</style>
