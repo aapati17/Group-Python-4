8418a459a48c9bf016a4336bbc4cd8033d74646a
agarasia <agarasia@asu.edu>
2025-02-17 19:18:29 -0700

    Enabled API Routing through Middleware

--- BackEnd/middleware/main.py
+++ BackEnd/middleware/main.py
@@ -1,9 +1,15 @@
+import sys
+sys.path.append('..')
 from fastapi import FastAPI, Form, File, UploadFile
-# Adding CORS middleware from FastAPI
+from LCOM4.routers.lcom4_router import router as lcom4_router
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List, Optional
+from typing import Optional
+from fastapi.testclient import TestClient
 
 app = FastAPI()
+
+# Include the LCOM4 router
+app.include_router(lcom4_router, prefix="/api/lcom4", tags=["LCOM4"])
 
 # Configure CORS
 app.add_middleware(
@@ -14,16 +20,24 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-@app.post("/sample")
+client = TestClient(app)
+
+@app.post("/")
 async def root(
     sourceType: str = Form(...),
     githubLink: Optional[str] = Form(None),
     file: Optional[UploadFile] = File(None),
     metrics: str = Form(...)
 ):
-    return {
-        "sourceType": sourceType,
-        "githubLink": githubLink,
-        "file": file,
-        "metrics": metrics
-    }
+    selectedMetrics = metrics.split(',')
+    if 'LCOM4' in selectedMetrics:
+        formData = {
+            "sourceType": sourceType,
+            "gitHubLink": githubLink,
+        }
+        response = client.post("/api/lcom4/calculate", data=formData)
+        return response.json()
+    else:
+        return {
+            "message": "No metrics selected."
+        }
